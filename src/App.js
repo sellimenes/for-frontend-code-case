9c00cd7cbb276815dd6a0bc00e6585d07fe47095
sellimenes <sellim81@gmail.com>
2022-06-29 22:59:36 +0300

    Hooklar Eklendi
    Hooklar eklendi. Beynim sulandı. Sonra devam etmek üzere kapatıldı.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Cards from "./Components/Cards";
 function App() {
+  const [pageNumber, letPageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      console.log(data);
+    })();
+  }, [api]);
+
   return (
     <>
       <Header />
