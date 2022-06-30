be3b397240a6beac8c5ad63c6e62dade390b1e05
Selim Enes Erdogan <sellimenes@users.noreply.github.com>
2022-06-30 11:49:54 +0300

    Kartlar düzenlendi
    Kartlar tasarım olarak düzenlendi. Status eklendi.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,12 +3,27 @@ import styles from "./Card.module.css";
 
 const Card = ({results}) => {
   const {name, status, species, image, location} = results; // Results deconstruction
+  
+  let statusColor;
+  let charStatus;
+  if(status === "Alive"){
+    charStatus =  <p className={`${styles.charStatus} ${styles.bgGreen}`}>{status}</p>
+  } else if(status === "Dead"){
+    charStatus =  <p className={`${styles.charStatus} ${styles.bgRed}`}>{status}</p>
+  } else{
+    charStatus =  <p className={`${styles.charStatus} ${styles.bgGrey}`}>{status}</p>
+  }
+  
   return <div className={styles.card}>
-    <img src={image} alt={name} />
+    <div className={styles.charImage}>
+      <img src={image} alt={name} />
+      {/* <p className={`${styles.charStatus}`}>{status}</p> */}
+      {charStatus}
+    </div>
     <div className={styles.meta}>
-    <h2>{name}</h2>
-    <small>Last Known Location</small>
-    <h3>{location.name}</h3>
+      <h2>{name}</h2>
+      <small>Last Known Location</small>
+      <h3>{location.name}</h3>
     </div>
   </div>;
 };
