e45afb84d5723800e6aa47e0ecae24c8d99937c4
Selim Enes Erdogan <sellimenes@users.noreply.github.com>
2022-06-30 08:34:42 +0300

    API eklendi ve bir değişkene atandı
    API eklendi, fetch edildi, bir useState değişkenine atıldı. fetchedData.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Header from "./Components/Header";
 import Cards from "./Components/Cards";
 function App() {
   const [pageNumber, letPageNumber] = useState(1);
+  const[fetchedData, setFetchedData] = useState([])
+  const {info, results} = fetchedData;
+
   let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
 
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
-      console.log(data);
+      setFetchedData(data);
     })();
   }, [api]);
 
