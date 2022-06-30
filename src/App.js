54af68bf31bd8647c6fcaaea2ceb6829ace76b73
Selim Enes Erdogan <sellimenes@users.noreply.github.com>
2022-06-30 09:31:22 +0300

    Cards props geçildi.
    Cards component'ine API.results prop geçildi. Cards içerisine tüm karakter kartları listelenecek.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
       <Header />
-      <Cards />
+      <Cards results={results} />
     </>
   );
 }
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Cards.module.css";
 
-const Cards = () => {
-  return <div>Cards</div>;
+const Cards = ({results}) => {
+
+  let display;
+  if(results){
+    display = `${results.length} karakter bulundu.`
+  }else{
+    display = "Aramanız ile eşleşen bir karakter bulunamadı."
+  }
+
+  return <div>{display}</div>;
 };
 
 export default Cards;
